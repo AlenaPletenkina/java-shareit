33d10301f412c1bac4f0440ba50887916acabab7
Alena Pletenkina <alenapletenkina@yandex.ru>
2024-10-13 14:12:20 +0300

    Спринт 14. add-controllers

---
+++ src/main/java/ru/practicum/shareit/item/services/ItemService.java
@@ -0,0 +1,19 @@
+package ru.practicum.shareit.item.services;
+
+import org.springframework.stereotype.Service;
+import ru.practicum.shareit.item.dto.ItemDto;
+
+import java.util.List;
+@Service
+
+public interface ItemService {
+    ItemDto addItem(long userId, ItemDto item);
+
+    ItemDto updateItem(long userId, long itemId, ItemDto itemDto);
+
+    ItemDto getItemDto(long itemId);
+
+    List<ItemDto> getAllItemsUser(long userId);
+
+    List<ItemDto> getSearchOfText(String text);
+}
