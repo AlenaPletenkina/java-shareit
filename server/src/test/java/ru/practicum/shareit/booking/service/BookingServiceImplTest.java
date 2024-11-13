5cffe6109faabde49f839cd3f2a1c550c42ceb91
Alena Pletenkina <alenapletenkina@yandex.ru>
2024-11-13 16:02:38 +0300

    Спринт 16. пробный 12

--- server/src/test/java/ru/practicum/shareit/booking/service/BookingServiceImplTest.java
+++ server/src/test/java/ru/practicum/shareit/booking/service/BookingServiceImplTest.java
@@ -3,7 +3,6 @@ package ru.practicum.shareit.booking.service;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
