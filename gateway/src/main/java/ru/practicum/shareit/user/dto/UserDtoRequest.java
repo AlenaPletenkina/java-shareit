82e476fa36bf8a33a0da4fb14a0a595dcd9defdc
Alena Pletenkina <alenapletenkina@yandex.ru>
2024-11-16 14:49:13 +0300

    Спринт 16. пробный 29

--- gateway/src/main/java/ru/practicum/shareit/user/dto/UserDtoRequest.java
+++ gateway/src/main/java/ru/practicum/shareit/user/dto/UserDtoRequest.java
@@ -3,8 +3,10 @@ package ru.practicum.shareit.user.dto;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.Pattern;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NonNull;
 import org.hibernate.sql.Update;
 import ru.practicum.shareit.validation.Create;
 
@@ -17,7 +19,6 @@ import java.util.Objects;
 public class UserDtoRequest {
     private final Long id;
     @NotBlank(groups = {Create.class})
-    @Pattern(regexp = "[a-zA-Zа-яА-Я\\s]*", groups = {Create.class, Update.class})
     private final String name;
     @NotEmpty(groups = {Create.class})
     @Email(groups = {Create.class, Update.class})
